5513e0855d02b7f82f4eca17e5ec098d2661f693
Angela Wong <celestialhearts05200615@gmail.com>
2026-07-21 09:14:30 +0800

    UpdatedComposition

---
+++ Assets/Script/CompositionScripts/ChordSlot.cs
@@ -0,0 +1,74 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ChordSlot : MonoBehaviour, IDropHandler, IPointerClickHandler
+{
+    [Header("Display")]
+    [SerializeField] private TMP_Text chordNameText;
+
+    [Header("Colours")]
+    [SerializeField] private Color emptyColour = new Color(1f, 1f, 1f, 0.25f);
+    [SerializeField] private Color occupiedColour = Color.white;
+
+    private ChordData assignedChord;
+
+    public ChordData AssignedChord => assignedChord;
+
+    //private void Awake()
+    //{
+    //    RefreshDisplay();
+    //}
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        // Check if something was dropped.
+        if (eventData.pointerDrag == null)
+            return;
+
+        ChordDragItem draggedChord =
+            eventData.pointerDrag.GetComponent<ChordDragItem>();
+
+        if (draggedChord == null || draggedChord.Data == null)
+            return;
+
+        AssignChord(draggedChord.Data);
+    }
+
+    public void AssignChord(ChordData chord)
+    {
+        // Save the chord into this slot.
+        assignedChord = chord;
+        RefreshDisplay();
+    }
+
+    public void ClearSlot()
+    {
+        // Remove the chord.
+        assignedChord = null;
+        RefreshDisplay();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        // Right click to clear the slot.
+        if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            ClearSlot();
+        }
+    }
+
+    private void RefreshDisplay()
+    {
+        // Update the text shown in the slot.
+        bool hasChord = assignedChord != null;
+
+        if (chordNameText != null)
+        {
+            chordNameText.text =
+                hasChord ? assignedChord.chordName : "Drop Chord";
+        }
+
+    }
+}
